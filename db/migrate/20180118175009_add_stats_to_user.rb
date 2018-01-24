882afe7d4db72847a4496255317711025066259f
Iris Inami <irisinami@Iriss-Mac.local>
2018-01-24 15:48:05 -0800

    adding word count to user

--- db/migrate/20180118175009_add_stats_to_user.rb
+++ db/migrate/20180118175009_add_stats_to_user.rb
@@ -3,7 +3,7 @@ class AddStatsToUser < ActiveRecord::Migration[5.0]
     add_column :users, :identifier, :string
     add_index :users, :identifier, unique: true
     add_column :users, :role, :string, default: 'user'
-    add_column :users, :blurb, :string
+    add_column :users, :bio, :string
 
     add_column :users, :level, :integer, default: 1
     add_column :users, :experience, :integer, default: 0
@@ -12,6 +12,7 @@ class AddStatsToUser < ActiveRecord::Migration[5.0]
     add_column :users, :referral_count, :integer, default: 0
     add_column :users, :friend_count, :integer, default: 0
     add_column :users, :story_count, :integer, default: 0
+    add_column :users, :word_count, :integer, default: 0
     add_column :users, :comment_count, :integer, default: 0
     add_column :users, :story_view_count, :integer, default: 0
     add_column :users, :profile_view_count, :integer, default: 0
