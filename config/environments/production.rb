7470aa184c16ceea99ae0338acb5a6f9364b606c
Iris Inami <irisinami@Iriss-Mac.local>
2018-02-05 18:14:16 -0800

    changing production.rb for websockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.disable_request_forgery_protection = true
+
+  config.action_cable.allowed_request_origins = [/.*/]
+  config.web_socket_server_url = "ws://18.220.223.89/cable"
 end
